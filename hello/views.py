49b2fd4dd5a97cc2bee4c11f5f7829f4d2959b58
kh-aos <sunghoonkritesh49@gmail.com>
2021-06-28 00:27:25 +0400

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
-import urllib
-import json
-import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -12,25 +9,17 @@ from bs4 import BeautifulSoup
 def index(request):
     URL = 'https://ceb.mu/customer-corner/power-outage-information'
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.content, 'html.parser')
     scripts = soup.find_all('script')
-
     data=scripts[14]
-    #print(data)
-
-
     result = scripts[14].decode(eventual_encoding='utf-8')
     result1 = result.replace("\/","/")
-
-    #print(result1)
-
     start =result1.find('{')+1
     end = result1.find('}')
     finaldata = result1[start:end]
+    print(finaldata)
     return HttpResponse(finaldata)
     #return render(request, "index.html")
-
 
 def db(request):
 
