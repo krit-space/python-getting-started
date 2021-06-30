224bfa6888bc68de928205f19e3fdd5ec7435f64
Kritesh Sunghoon <80051957+krit-space@users.noreply.github.com>
2021-06-30 10:18:06 +0400

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
 ]
 
 ROOT_URLCONF = "gettingstarted.urls"
