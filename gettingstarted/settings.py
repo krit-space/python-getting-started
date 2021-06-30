2583ebf71f758dcd36b6efec768ac90e52bc13fa
Kritesh Sunghoon <80051957+krit-space@users.noreply.github.com>
2021-06-30 10:18:48 +0400

    Update settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -118,4 +118,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
+
+ALLOWED_HOSTS=['*']
+CORS_ORIGIN_ALLOW_ALL = True
+
 django_heroku.settings(locals())
